36a7ea936fb1f551ef255850e521c0f92fda8451
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-01-07 15:49:43 +0700

    add build

---
+++ backend/build/app.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var db_1 = __importDefault(require("./config/db"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var cors_1 = __importDefault(require("cors"));
+var index_1 = __importDefault(require("./routes/index"));
+var urls_1 = __importDefault(require("./routes/urls"));
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+(0, db_1.default)();
+// Body Parser
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: process.env.corsOrigin
+}));
+app.get("/", function (req, res) {
+    res.send("welcome to stthi shortUrl backend");
+});
+app.use("/", index_1.default);
+app.use("/api", urls_1.default);
+// if ((process.env.NODE_ENV === "production")) {
+//   app.use(express.static(path.join(__dirname, "../build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build/index.html"));
+//   });
+// }
+// Server Setup
+var PORT = process.env.PORT || 3333;
+app.listen(PORT, function () {
+    console.log("Server is running at PORT ".concat(PORT));
+});
---
+++ backend/build/models/Url.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = __importDefault(require("mongoose"));
+var UrlSchema = new mongoose_1.default.Schema({
+    urlId: {
+        type: String,
+        required: true,
+    },
+    origUrl: {
+        type: String,
+        required: true,
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    date: {
+        type: String,
+        default: Date.now,
+    },
+});
+exports.default = mongoose_1.default.model('Url', UrlSchema);
